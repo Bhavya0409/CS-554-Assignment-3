aa10414dbbea49f8589f2050f1447a84a471d3d2
Bhavya Shah <bhavyashah@Bhavyas-MBP.home>
2018-09-16 19:38:04 -0400

    added tota11y, but didnt wire it up yet

--- app/js/modal.js
+++ app/js/modal.js
@@ -1,9 +1,8 @@
 $('#exampleModal').on('show.bs.modal', function(event) {
-	console.log('test');
-	const button = $(event.relatedTarget) // Button that triggered the modal
-	const description = button.data('description') // Extract info from data-* attributes
+	const button = $(event.relatedTarget); // Button that triggered the modal
+	const description = button.data('description'); // Extract info from data-* attributes
 	const title = button.data('title');
-	const modal = $(this)
-	modal.find('.modal-title').text(title)
-	modal.find('.modal-body').text(description)
+	const modal = $(this);
+	modal.find('.modal-title').text(title);
+	modal.find('.modal-body').text(description);
 })
